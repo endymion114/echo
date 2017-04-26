96c79db02cf66a7b271176fb55fe958d6eb8d519
endymion114 <ebcc200089@gmail.com>
2017-04-26 13:19:22 -0500

    Update server.py

--- server.py
+++ server.py
@@ -4,21 +4,36 @@ import os
 from flask import Flask
 from flask_ask import Ask, request, session, question, statement
 
+
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
 
 @ask.launch
 def launch():
     speech_text = 'Welcome to the Alexa Skills Kit, you can say hello'
     return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
-    
-    @ask.intent('HelloWorldIntent')
+
+
+@ask.intent('HelloWorldIntent')
 def hello_world():
     speech_text = 'Hello world'
     return statement(speech_text).simple_card('HelloWorld', speech_text)
-    
-    if __name__ == '__main__':
+
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    speech_text = 'You can say hello to me!'
+    return question(speech_text).reprompt(speech_text).simple_card('HelloWorld', speech_text)
+
+
+@ask.session_ended
+def session_ended():
+    return "{}", 200
+
+
+if __name__ == '__main__':
     if 'ASK_VERIFY_REQUESTS' in os.environ:
         verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
         if verify == 'false':
